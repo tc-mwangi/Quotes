5e05f946a422b7bea9ee0063244854b95667f0f2
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-01-16 18:36:18 +0300

    fix object error

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,71 +2,69 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 // import quote class
 import { Quote }    from '../quote'; 
-
+ 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  
 
     // option tag loop options
     options = [
-      {category: ''},
-      {category: 'Life'},
-      {category: 'Love'},
-      {category: 'Work'},
-      {category: 'Religion'},
-      {category: 'Famous'},
-      {category: 'Miscellaneous'}
+      'Life',
+      'Love',
+      'Work',
+      'Religion',
+      'Famous',
+      'Miscellaneous'
   
     ];
 
-        // object to loop through loop quotes
-        // displayQuotes = [
-        //   {quote: ''},
-        //   {author: ''}
-        // ];
-
-
-
-  
-
     // default object to be changed by form inputs
     
-    model = new Quote(1, '', '', '','', 'new Date(2018,3,14)');
+    model = new Quote(1, 'Get busy living or get busy dying.', 'Stephen King', 'Kelly Morgan','option', 'new Date(2018,3,14)');
 
-
-    @Output() addQuote=new EventEmitter<Quote>();
-
-    submitQuote(){
-      this.addQuote.emit(this.model);
-
-    }
-
+    // hide and show
     submitted = false;
 
-    onSubmit(){this.submitted = true;}
+    onSubmit() {this.submitted = true;}
 
-    // when this button is clicked inputs to be displayed on quotes bar
+    // Add new quotes
     newQuote(){
 
-      this.model = new Quote(0, '', '', '', '', 'new Date(2018,3,14)')
+      this.model = new Quote(7, '', '', '', '', 'new Date(2018,3,14)')
     }
+
+
+
+
+    // @Output() addQuote=new EventEmitter<Quote>();
+
+    // submitQuote(){
+    //   this.addQuote.emit(this.model);
+
+    // }
+
+   
+
+    
+
+    // when this button is clicked inputs to be displayed on quotes bar
+
     
 
 
-    publishQuote(){
+    // publishQuote(){
 
-      this.addQuote.emit(this.model);
+    //   this.addQuote.emit(this.model);
 
-    }
+    // }
 
-    showAlert(){
+    // showAlert(){
       
 
-    }
+    // }
 
 
   //   deleteGoal(index){
@@ -91,3 +89,7 @@ export class QuoteFormComponent implements OnInit {
   }
 
 }
+
+
+
+
